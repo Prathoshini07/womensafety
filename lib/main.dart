78d9ebfc38fe1e9e68f0d0ef2d75a71c956a997d
Prathoshini07 <prathoshini07@gmail.com>
2024-02-03 01:38:03 +0530

    Bg image and login button

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( 
-    MaterialApp
-    (
-      title:'Women Safety App',
-      theme:ThemeData(primarySwatch: Colors.blue),
-    home : const HomePage(),
+  runApp(
+    MaterialApp(
+      title: 'Women Safety App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
     ),
   );
 }
+
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}):super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(32),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("web/assets/image1.jpg"),
+                fit : BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: TextButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
-
-
